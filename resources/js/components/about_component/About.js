6ee197c0a9d131a1bef36cf740a035bc75a912b5
Kirill2602 <kaspicin2602@gmail.com>
2022-03-25 15:07:49 +0300

    main page

--- resources/js/components/about_component/About.js
+++ resources/js/components/about_component/About.js
@@ -1,7 +1,29 @@
+import about from './assets/about-pets.png'
+import './about.css'
+
 export const About = () => {
     return (
-        <div>
-            ABOUT
+        <div className='about__wrapper'>
+            <div className='about__img__block'><img src={about} alt="about_pets"/></div>
+            <div className='about__content'>
+                <div className='about__title__block'><h3 className='about__title'>About the shelter “Cozy House”</h3></div>
+                <div className='about__parag__block top_parag'>
+                    <p className='about__parag '>Currently we have 121 dogs and 342 cats on our hands and statistics show that only 20% of them
+                        will find a family. The others will continue to live with us and will be waiting for a lucky
+                        chance
+                        to become dearly loved.
+                    </p>
+                </div>
+                <div className='about__parag__block'>
+                    <p className='about__parag'>We feed our wards with the best food and make sure that they do not get sick, feel comfortable
+                        (including psychologically) and well. We are supported by 87 volunteers and 28 employees of
+                        various
+                        skill levels. About 12% of the animals are taken by the shelter staff. Taking care of the
+                        animals,
+                        they become attached to the pets and would hardly ever leave them alone.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
--- resources/js/components/help_shelter_component/Help.js
+++ resources/js/components/help_shelter_component/Help.js
@@ -1,7 +1,33 @@
+import './help.css'
+import vector from './assets/images/Vector.png'
+import medicines from './assets/images/medicines.png'
+import shampoos from './assets/images/shampoos.png'
+import sleeping from './assets/images/sleeping-area.png'
+import toys from './assets/images/toys.png'
+import transform from './assets/images/transportation.png'
+import vitamins from './assets/images/vitamins.png'
+import bowls from './assets/images/bowls-and-cups.png'
+import collars from './assets/images/collars-or-leashes.png'
+
 export const Help = () => {
     return (
-        <div>
-            HELP
+        <div className='help__wrapper'>
+            <div className='help__title_block'>
+                <h3 className='help__title'>How you can help our shelter</h3>
+            </div>
+            <div className='help__line'>
+                <div className='help__block'><img className='help__img food' src={vector} alt='Picture'/><h4 className='help__parag'>Pet food</h4></div>
+                <div className='help__block'><img className='help__img' src={transform} alt='Picture'/><h4 className='help__parag'>Transportation</h4></div>
+                <div className='help__block'><img className='help__img' src={toys} alt='Picture'/><h4 className='help__parag'>Toys</h4></div>
+                <div className='help__block'><img className='help__img' src={bowls} alt='Picture'/><h4 className='help__parag'>Bowls and cups</h4></div>
+                <div className='help__block'><img className='help__img shampo' src={shampoos} alt='Picture'/><p h4 className='help__parag'>Shampoos</p></div>
+            </div>
+            <div className='help__line'>
+                <div className='help__block'><img className='help__img' src={vitamins} alt='Picture'/><h4 className='help__parag'>Vitamins</h4></div>
+                <div className='help__block'><img className='help__img' src={medicines} alt='Picture'/><h4 className='help__parag'>Medicines</h4></div>
+                <div className='help__block'><img className='help__img' src={collars} alt='Picture'/><h4 className='help__parag'>Collars / leashes</h4></div>
+                <div className='help__block'><img className='help__img' src={sleeping} alt='Picture'/><h4 className='help__parag'>Sleeping areas</h4></div>
+            </div>
         </div>
     )
 }

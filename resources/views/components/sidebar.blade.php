bf182361765ab8ed5165c2fafa4d4696c8b2bc74
Petr Komlev <petr.komlev@gmail.com>
2022-03-31 21:39:12 +0300

    Add nav-link component in sidebar component

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,41 +2,12 @@
   <div class="position-sticky pt-3">
 
     <ul class="nav flex-column">
-      @if (Route::has('admin.dashboard'))
-      <li class="nav-item">
-        <a class="nav-link @if (request()->routeIs('admin.dashboard')) active @endif" href="{{ route('admin.dashboard') }}">Dashboard</a>
-      </li>
-      @endif
-
-      @if (Route::has('admin.animals.index'))
-      <li class="nav-item">
-        <a class="nav-link @if (request()->routeIs('admin.animals.*')) active @endif" href="{{ route('admin.animals.index') }}">Animals</a>
-      </li>
-      @endif
-
-      @if (Route::has('admin.breeds.index'))
-      <li class="nav-item">
-        <a class="nav-link @if (request()->routeIs('admin.breeds.*')) active @endif" href="{{ route('admin.breeds.index') }}">Breeds</a>
-      </li>
-      @endif
-
-      @if (Route::has('admin.animal_types.index'))
-      <li class="nav-item">
-        <a class="nav-link @if (request()->routeIs('admin.animal_types.*')) active @endif" href="{{ route('admin.animal_types.index') }}">Types</a>
-      </li>
-      @endif
-
-      @if (Route::has('admin.diseases.index'))
-      <li class="nav-item">
-        <a class="nav-link @if (request()->routeIs('admin.diseases.*')) active @endif" href="{{ route('admin.diseases.index') }}">Diseases</a>
-      </li>
-      @endif
-
-      @if (Route::has('admin.inoculations.index'))
-      <li class="nav-item">
-        <a class="nav-link @if (request()->routeIs('admin.inoculations.*')) active @endif" href="{{ route('admin.inoculations.index') }}">Inoculations</a>
-      </li>
-      @endif
+      <x-nav-link href="admin.dashboard" route="admin.dashboard">Dashboard</x-nav-link>
+      <x-nav-link href="admin.animals.index" route="admin.animals.*">Aniamls</x-nav-link>
+      <x-nav-link href="admin.breeds.index" route="admin.breeds.*">Breeds</x-nav-link>
+      <x-nav-link href="admin.animal_types.index" route="admin.animal_types.*">Types</x-nav-link>
+      <x-nav-link href="admin.diseases.index" route="admin.diseases.*">Diseases</x-nav-link>
+      <x-nav-link href="admin.inoculations.index" route="admin.inoculations.*">Inoculations</x-nav-link>
 
       <li class="nav-item d-block d-md-none">
         <a class="nav-link" href="#">Sign out</a>

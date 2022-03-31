cbac73ed42efa846bb9009abab0fb677b6b02f1f
Petr Komlev <petr.komlev@gmail.com>
2022-03-31 10:07:59 +0300

    Add components on pages

--- resources/views/admin/animals/create.blade.php
+++ resources/views/admin/animals/create.blade.php
@@ -1,48 +1,76 @@
-<form method="post" action="{{ route('admin.animals.store')}}" enctype="multipart/form-data">
-    @csrf
-    <label for="type_id">Тип</label>
+<x-layout>
+  <x-slot name="title">
+    Animals create
+  </x-slot>
+
+  @if ($errors->any())
+    <x-alert type="danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </x-alert>
+  @endif
+
+  <x-form method="POST" action="{{ route('admin.animals.store') }}" enctype="multipart/form-data">
+    <div class="mb-3">
+      <x-label for="type_id">Тип</x-label>
     <select id="type_id" name="type_id">
         @foreach($animal_types as $animal_typesItem)
             <option value="{{ $animal_typesItem->id }}"
                     @if(old('type_id') === $animal_typesItem->id) selected @endif>{{ $animal_typesItem->name }}</option>
         @endforeach
     </select>
-    <br>
-    <label for="breed_id">Порода</label>
+    </div>
+    <div class="mb-3">
+      <x-label for="breed_id">Порода</x-label>
     <select id="breed_id" name="breed_id">
         @foreach($breeds as $breedsItem)
             <option value="{{ $breedsItem->id }}"
                     @if(old('breed_id') === $breedsItem->id) selected @endif>{{ $breedsItem->name }}</option>
         @endforeach
     </select>
-    <br>
-    <label for="name">Имя</label>
-    <input type="text" id="name" name="name" value="{{ old('name') }}">
-    <br>
-    <label for="description">Описание</label>
-    <br>
+    </div>
+    <div class="mb-3">
+      <x-label for="name">Имя</x-label>
+      <x-input name="name" value="{{ old('name') }}" />
+    </div>
+    <div class="mb-3">
+      <x-label for="description">Описание</x-label>
     <textarea name="description" id="description" cols="30" rows="10">{{old('description')}}</textarea>
-    <br>
-    <label for="treatment_of_parasites">Паразиты</label>
+    </div>
+    <div class="mb-3">
+      <x-label>Паразиты</x-label>
+      <div class="form-check">
     <input type="radio" name="treatment_of_parasites" id="inp1" value="YES" @if(old('treatment_of_parasites')==="YES") checked @endif>
-    <label for="inp1">Да</label>
+      <x-label for="inp1" class="form-check-label">Да</x-label>
+      </div>
+      <div class="form-check">
     <input type="radio" name="treatment_of_parasites" id="inp2" value="NO" @if(old('treatment_of_parasites')==="NO") checked @endif>
-    <label for="inp1">Нет</label>
-    <br>
-    <p>Болезни</p>
+      <x-label for="inp2" class="form-check-label">Нет</x-label>
+      </div>
+    </div>
+    <div class="mb-3">
+      <x-label for="diseases">Diseases</x-label>
     <select id="diseases" name="diseases[]" multiple>
         @foreach($diseases as $diseasesItem)
             <option value="{{$diseasesItem->id}}">{{ $diseasesItem->name }}</option>
         @endforeach
     </select>
-    <p>Прививки</p>
+    </div>
+    <div class="mb-3">
+      <x-label for="inoculations">Inoculations</x-label>
     <select id="inoculations" name="inoculations[]" multiple>
         @foreach($inoculations as $inoculationsItem)
             <option value="{{$inoculationsItem->id}}">{{ $inoculationsItem->name }}</option>
         @endforeach
     </select>
-    <br>
-    <input type="date" name="birthday_at" value="{{old('birthday_at')}}">
-    <br>
-    <button type="submit">Сохранить</button>
-</form>
+    </div>
+    <div class="mb-3">
+      <x-label>Birthday</x-label>
+      <x-input type="date" name="birthday_at" value="{{old('birthday_at')}}" />
+    </div>
+    <x-button type="submit" color="outline-success" class="btn-sm">Сохранить</x-button>
+  </x-form>
+</x-layout>
--- resources/views/admin/animals/index.blade.php
+++ resources/views/admin/animals/index.blade.php
@@ -1,32 +1,66 @@
-<a href="{{route('admin.animals.create')}}">Создать</a>
+<x-layout>
+  <x-slot name="title">
+    Animals
+  </x-slot>
+
+  <x-slot name="toolbar">
+    @if (Route::has('admin.animals.create'))
+      <a href="{{ route('admin.animals.create') }}" class="btn btn-sm btn-outline-success">Create</a>
+    @endif
+  </x-slot>
+
+  @if (session('success'))
+    <x-alert type="success">
+      {{ session('success') }}
+    </x-alert>
+  @endif
+  
+  <x-table>
+    <x-slot name="header">
+      <th>#</th>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Type</th>
+      <th>Breed</th>
+      <th>Diseases</th>
+      <th>Inoculations</th>
+      <th>Parasites</th>
+      <th>Birthday</th>
+      <th>Images</th>
+      <th></th>
+    </x-slot>
 @foreach($animals as $animalsItem)
-    <p>{{$animalsItem->id}}</p>
-    <p>Имя {{$animalsItem->name}}</p>
-    <p>Описание {{$animalsItem->description}}</p>
-    <p>Тип {{$animalsItem->type->name}}</p>
-    <p>Порода {{$animalsItem->breed->name}}</p>
-    <div> Болезни:
+      <tr>
+        <td>{{$animalsItem->id}}</td>
+        <td>{{$animalsItem->name}}</td>
+        <td>{{$animalsItem->description}}</td>
+        <td>{{$animalsItem->type->name}}</td>
+        <td>{{$animalsItem->breed->name}}</td>
+        <td>
         @foreach($animalsItem->disease AS $diseaseItem)
-           <p>{{$diseaseItem->name}}</p>
+          <p>{{$diseaseItem->name}}</p>
         @endforeach
-    </div>
-    <div> Прививки:
+        </td>
+        <td>
         @foreach($animalsItem->inoculation AS $inoculationItem)
             <p>{{$inoculationItem->name}}</p>
         @endforeach
-    </div>
-    <p>{{$animalsItem->treatment_of_parasites}}</p>
-    <p>{{$animalsItem->birthday_at}}</p>
-    <div> Фото:
+        </td>
+        <td>{{$animalsItem->treatment_of_parasites}}</td>
+        <td>{{$animalsItem->birthday_at}}</td>
+        <td>
         @foreach($animalsItem->images AS $imagesItem)
             <img src="{{$imagesItem->path}}" alt="#">
         @endforeach
-    </div>
+        </td>
+        <td>
     <a href="{{route('admin.animals.edit', ['animal' => $animalsItem])}}">Редактировать</a>
-    <form method="post" action="{{ route('admin.animals.destroy', ['animal' => $animalsItem])}}">
-        @csrf
-        @method('delete')
-        <button type="submit">Удалить</button>
-    </form>
-    <hr>
+        <x-form method="POST" action="{{ route('admin.animals.destroy', $animalsItem) }}">
+            @method('DELETE')
+            <x-button type="submit" color="outline-danger" class="btn-sm">Удалить</x-button>
+        </x-form>
+        </td>
+      </tr>
 @endforeach
+  </x-table>
+</x-layout>

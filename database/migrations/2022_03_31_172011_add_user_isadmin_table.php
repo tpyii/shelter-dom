b29bbec186d2c5e49b17e86c6f8d0f5b222fc909
Igor_Gruzdo <igruzdo@yandex.ru>
2022-03-31 20:39:29 +0300

    add migration for user table

---
+++ database/migrations/2022_03_31_172011_add_user_isadmin_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIsAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('is_admin')->default(false)->after('password');
+            $table->string('session_token')->unique()->after('is_admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}

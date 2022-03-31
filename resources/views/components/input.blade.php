ce7feac57529b8cbcd1bfe7b617ada041f13d17e
Petr Komlev <petr.komlev@gmail.com>
2022-03-31 13:06:11 +0300

    Add radio and checkbox type input component

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,12 +1,57 @@
-<input class="form-control form-control-sm @error($name) is-invalid @enderror" 
-{{ $attributes->merge([
+@props([
   'type' => 'text',
-  'id' => $name,
-  'value' => old($name)
-]) }}>
+  'value' => '',
+  'checked' => '',
+  'name',
+  'id',
+  'label',
+])
 
-@error($name)
-  <div class="invalid-feedback">
-    {{ $message }}
-  </div>
-@enderror
+@switch($type)
+  @case('radio')
+  @case('checkbox')
+    <div class="form-check">
+      <input 
+        {{ $attributes->merge([
+          'class' => 'form-check-input',
+          'type' => $type,
+          'name' => $name,
+          'id' => $id,
+          'value' => old($name, $value),
+        ])->class([
+          'is-invalid' => $errors->has($name),
+        ]) }}
+        @if(old($name, $checked) === $value) checked @endif
+      >
+      
+      <x-label for="{{ $id }}">
+        {{ $label }}
+      </x-label>
+
+      @error($name)
+        <div class="invalid-feedback mb-3">
+          {{ $message }}
+        </div>
+      @enderror
+    </div>
+  @break
+
+  @default
+    <input 
+      {{ $attributes->merge([
+        'class' => 'form-control form-control-sm',
+        'type' => $type,
+        'name' => $name,
+        'id' => $name,
+        'value' => old($name, $value)
+      ])->class([
+        'is-invalid' => $errors->has($name),
+      ]) }}
+    >
+    
+    @error($name)
+      <div class="invalid-feedback">
+        {{ $message }}
+      </div>
+    @enderror
+@endswitch

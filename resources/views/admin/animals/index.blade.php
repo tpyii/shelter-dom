ea2256abbcea82172d5ed17b55ffa23259fd649b
Petr Komlev <petr.komlev@gmail.com>
2022-04-07 14:22:15 +0300

    Add filter modal on animals page

--- resources/views/admin/animals/index.blade.php
+++ resources/views/admin/animals/index.blade.php
@@ -4,44 +4,14 @@
     </x-slot>
 
     <x-slot name="toolbar">
+        <x-button color="outline-success" data-bs-toggle="modal" data-bs-target="#filter">
+            Filter
+        </x-button>
+
         @if (Route::has('admin.animals.create'))
             <a href="{{ route('admin.animals.create') }}" class="btn btn-sm btn-outline-success">Create</a>
         @endif
     </x-slot>
-
-    <x-form method="GET" action="{{ route('admin.animals.index') }}" enctype="multipart/form-data" class="w-25">
-        @if($searchParams)
-            @if(array_key_exists('name',$searchParams))
-                <x-input name="name" label="Имя" value="{{ $searchParams['name'] }}"/>
-            @else
-                <x-input name="name" label="Имя" value=""/>
-            @endif
-            @if(array_key_exists('type_id',$searchParams))
-                <x-select name="type_id" label="Тип" :options="$animal_types" value="{{ $searchParams['type_id'] }}"/>
-            @else
-                <x-select name="type_id" label="Тип" :options="$animal_types" value=""/>
-            @endif
-            @if(array_key_exists('breed_id',$searchParams))
-                <x-select name="breed_id" label="Порода" :options="$breeds" value="{{ $searchParams['breed_id'] }}"/>
-            @else
-                <x-select name="breed_id" label="Порода" :options="$breeds" value=""/>
-            @endif
-        @else
-            <x-input name="name" label="Имя" value=""/>
-            <x-select name="type_id" label="Тип" :options="$animal_types" value=""/>
-            <x-select name="breed_id" label="Порода" :options="$breeds" value=""/>
-        @endif
-        {{--        <div class="mb-3">--}}
-        {{--            <x-label for="inp1">Паразиты</x-label>--}}
-        {{--            <x-input type="radio" name="treatment_of_parasites" id="inp1" value="YES" label="Да" />--}}
-        {{--            <x-input type="radio" name="treatment_of_parasites" id="inp2" value="NO" label="Нет" />--}}
-        {{--        </div>--}}
-        {{--        <x-select name="diseases[]" label="Diseases" :options="$diseases" multiple />--}}
-        {{--        <x-select name="inoculations[]" label="Inoculations" :options="$inoculations" multiple />--}}
-        {{--        <x-input type="date" name="birthday_at" label="Birthday" value="{{ old('birthday_at') }}" />--}}
-        <x-button type="submit" color="outline-success mb-4">Search</x-button>
-    </x-form>
-
 
     <x-success/>
 
@@ -99,4 +69,42 @@
     @if(method_exists($animals, 'links'))
         {{$animals->links()}}
     @endif
+
+    <x-modal id="filter" title="Filter">
+        <x-form action="{{ route('admin.animals.index') }}">
+            @if($searchParams)
+                @if(array_key_exists('name',$searchParams))
+                    <x-input name="name" label="Имя" value="{{ $searchParams['name'] }}"/>
+                @else
+                    <x-input name="name" label="Имя" value=""/>
+                @endif
+                @if(array_key_exists('type_id',$searchParams))
+                    <x-select name="type_id" label="Тип" :options="$animal_types" value="{{ $searchParams['type_id'] }}"/>
+                @else
+                    <x-select name="type_id" label="Тип" :options="$animal_types" value=""/>
+                @endif
+                @if(array_key_exists('breed_id',$searchParams))
+                    <x-select name="breed_id" label="Порода" :options="$breeds" value="{{ $searchParams['breed_id'] }}"/>
+                @else
+                    <x-select name="breed_id" label="Порода" :options="$breeds" value=""/>
+                @endif
+            @else
+                <x-input name="name" label="Имя" value=""/>
+                <x-select name="type_id" label="Тип" :options="$animal_types" value=""/>
+                <x-select name="breed_id" label="Порода" :options="$breeds" value=""/>
+            @endif
+            {{--        <div class="mb-3">--}}
+            {{--            <x-label for="inp1">Паразиты</x-label>--}}
+            {{--            <x-input type="radio" name="treatment_of_parasites" id="inp1" value="YES" label="Да" />--}}
+            {{--            <x-input type="radio" name="treatment_of_parasites" id="inp2" value="NO" label="Нет" />--}}
+            {{--        </div>--}}
+            {{--        <x-select name="diseases[]" label="Diseases" :options="$diseases" multiple />--}}
+            {{--        <x-select name="inoculations[]" label="Inoculations" :options="$inoculations" multiple />--}}
+            {{--        <x-input type="date" name="birthday_at" label="Birthday" value="{{ old('birthday_at') }}" />--}}
+        </x-form>
+        <x-slot name="footer">
+            <a class="btn btn-sm btn-secondary" href="{{ route('admin.animals.index') }}">Reset</a>
+            <x-button type="submit" color="primary">Apply</x-button>
+        </x-slot>
+    </x-modal>
 </x-layout>

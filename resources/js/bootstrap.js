0e2deaa5aed608f0ddd7b410f81e902b05de5da1
Petr Komlev <petr.komlev@gmail.com>
2022-04-11 13:06:33 +0300

    Add scripts for delete model actions

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,7 @@
 window._ = require('lodash');
 
 try {
-    require('bootstrap');
+    window.bootstrap = require('bootstrap');
 } catch (e) {}
 
 /**
@@ -32,3 +32,5 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // });
 
 require('./form');
+
+require('./delete');

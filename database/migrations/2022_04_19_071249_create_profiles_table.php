04f4c7710fbade3c4f76b7523f14ef7c4bad78ad
MatTwix <mfgolden@yandex.ru>
2022-04-19 13:12:54 +0300

    migrations and seeders for profiles

---
+++ database/migrations/2022_04_19_071249_create_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('phone')->unique()->index();
+            $table->string('name');
+            $table->string('surname');
+            $table->text('description');
+            $table->mediumText('address');
+            $table->date('birthday_at');
+            $table->string('avatar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}

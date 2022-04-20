2c05d34080d96d5712ddb9b5728b80f7c1673283
MatTwix <mfgolden@yandex.ru>
2022-04-20 10:06:28 +0300

    profiles.user_id now unique, adding dir with avatars

--- database/migrations/2022_04_19_071249_create_profiles_table.php
+++ database/migrations/2022_04_19_071249_create_profiles_table.php
@@ -15,7 +15,7 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->unique()->constrained();
             $table->string('phone')->unique()->index();
             $table->string('name');
             $table->string('surname');

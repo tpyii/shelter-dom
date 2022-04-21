471cf87e880e40471cc7d2ece6906769e943708a
Petr Komlev <petr.komlev@gmail.com>
2022-04-21 16:45:39 +0300

    Fix comments

--- app/Contracts/UploadService.php
+++ app/Contracts/UploadService.php
@@ -3,12 +3,11 @@
 namespace App\Contracts;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\UploadedFile;
 
 interface UploadService
 {
     /**
-     * @param array|UploadedFile $files
+     * @param array|\Illuminate\Http\UploadedFile $files
      * @param \Illuminate\Database\Eloquent\Model $model
      * @return void
      */
--- app/Services/UserImageUploadService.php
+++ app/Services/UserImageUploadService.php
@@ -3,13 +3,12 @@
 namespace App\Services;
 
 use App\Contracts\UploadService;
-use Illuminate\Http\UploadedFile;
 use Illuminate\Database\Eloquent\Model;
 
 class UserImageUploadService implements UploadService
 {
     /**
-     * @param UploadedFile $files
+     * @param \Illuminate\Http\UploadedFile $files
      * @param \Illuminate\Database\Eloquent\Model $model
      * @return void
      */

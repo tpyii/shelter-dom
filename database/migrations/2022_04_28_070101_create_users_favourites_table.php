fd0275ae21b3f838abc948c860cd682be2a7e218
MatTwix <mfgolden@yandex.ru>
2022-04-28 10:14:17 +0300

    renamed table user_favourites

--- database/migrations/2022_04_28_070101_create_users_favourites_table.php
+++ database/migrations/2022_04_28_070101_create_users_favourites_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersFavoritesAnimals extends Migration
+class CreateUsersFavouritesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersFavoritesAnimals extends Migration
      */
     public function up()
     {
-        Schema::create('users_favorites_animals', function (Blueprint $table) {
+        Schema::create('users_favourites', function (Blueprint $table) {
             $table->foreignId('user_id')->constrained();
             $table->foreignId('animal_id')->constrained();
         });
@@ -26,6 +26,6 @@ class CreateUsersFavoritesAnimals extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_favorites_animals');
+        Schema::dropIfExists('users_favourites');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,7 +42,7 @@ class DatabaseSeeder extends Seeder
             AnimalInoculationSeeder::class,
             AnimalDiseaseSeeder::class,
             AnimalImagesSeeder::class,
-            UserFavoritesSeeder::class,
+            UserFavouritesSeeder::class,
         ]);
     }
 }

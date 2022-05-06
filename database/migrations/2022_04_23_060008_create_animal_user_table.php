be41c532569a4a15b3248e6e4fc4f3d2e9e8a615
Petr Komlev <petr.komlev@gmail.com>
2022-05-06 11:43:41 +0300

    Fix merge

--- database/migrations/2022_04_23_060008_create_animal_user_table.php
+++ database/migrations/2022_04_23_060008_create_animal_user_table.php
@@ -4,11 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-<<<<<<< HEAD:database/migrations/2022_04_23_060008_create_animal_user_table.php
 class CreateAnimalUserTable extends Migration
-=======
-class CreateUsersFavouritesTable extends Migration
->>>>>>> test_react_branch:database/migrations/2022_04_28_070101_create_users_favourites_table.php
 {
     /**
      * Run the migrations.
@@ -17,11 +13,7 @@ class CreateUsersFavouritesTable extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD:database/migrations/2022_04_23_060008_create_animal_user_table.php
         Schema::create('animal_user', function (Blueprint $table) {
-=======
-        Schema::create('users_favourites', function (Blueprint $table) {
->>>>>>> test_react_branch:database/migrations/2022_04_28_070101_create_users_favourites_table.php
             $table->foreignId('user_id')->constrained();
             $table->foreignId('animal_id')->constrained();
         });
@@ -34,10 +26,6 @@ class CreateUsersFavouritesTable extends Migration
      */
     public function down()
     {
-<<<<<<< HEAD:database/migrations/2022_04_23_060008_create_animal_user_table.php
         Schema::dropIfExists('animal_user');
-=======
-        Schema::dropIfExists('users_favourites');
->>>>>>> test_react_branch:database/migrations/2022_04_28_070101_create_users_favourites_table.php
     }
 }
